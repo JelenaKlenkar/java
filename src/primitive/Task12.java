fb3e7a1ecf1d7245679a72723b6199556740ed5b
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-06-16 21:56:34 +0200

    added changes to Task12 and Task13

--- src/primitive/Task12.java
+++ src/primitive/Task12.java
@@ -2,4 +2,21 @@ package primitive;
 
 public class Task12 {
 
+	public static void main(String[] args) {
+		//
+		String str;
+		char a = 'B';
+		char b = 'u';
+		char c = 'b';
+		char d = 'i';
+		char e = 'c';
+		char f = 'a';
+		
+		str = new StringBuilder().append("").append(a).append(b).append(c).append(d).append(e).append(f).toString();
+
+		
+		System.out.println(str);
+
+	}
+
 }
--- src/primitive/Task13.java
+++ src/primitive/Task13.java
@@ -1,5 +1,19 @@
 package primitive;
 
 public class Task13 {
+	public static void main(String[] args) {
+		// using String builder to convert characters to String shorter version
 
+		String str;
+		char a = 'B';
+		char b = 'u';
+		char c = 'b';
+		char d = 'i';
+		char e = 'c';
+		char f = 'a';
+
+		str = "" + a + b + c + d + e + f;
+
+		System.out.println(str);
+	}
 }
