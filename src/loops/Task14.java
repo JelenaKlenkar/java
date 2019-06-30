26da752f24c74db51243138dbb73c54b392ef34c
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-06-30 22:48:36 +0200

    added Task14 and Task15

---
+++ src/loops/Task14.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Task14 {
+	public static void main(String[] args) {
+
+		// with while loop print out 7 14 21 28 35 42 49 56 63 70 77 84 91 98
+
+		int i = 7;
+
+		while (i <= 98) {
+			System.out.print(i + " ");
+			i += 7;
+		}
+
+	}
+
+}
---
+++ src/loops/Task15.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class Task15 {
+	public static void main(String[] args) {
+		// with while loop print out 1 2 4 8 16 32 64 128 256 512
+
+		int a = 1;
+
+		while (a <= 512) {
+			System.out.print(a + " ");
+			a *= 2;
+		}
+	}
+
+}
